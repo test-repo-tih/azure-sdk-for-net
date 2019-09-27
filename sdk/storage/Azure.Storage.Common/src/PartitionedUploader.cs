7f52505416cd477b6154d77024a78318a4864b13
Phoenix He <tih@microsoft.com>
2019-09-27 11:05:47 +0800

    Update swagger_to_sdk_config.json

--- sdk/storage/Azure.Storage.Common/src/PartitionedUploader.cs
+++ sdk/storage/Azure.Storage.Common/src/PartitionedUploader.cs
@@ -6,6 +6,7 @@ using System.Buffers;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Storage.Common;
 
 namespace Azure.Storage
 {
@@ -90,7 +91,7 @@ namespace Azure.Storage
                 parallelTransferOptions ??= new ParallelTransferOptions();
 
                 var maximumThreadCount =
-                    parallelTransferOptions.Value.MaximumConcurrency ?? Constants.Blob.Block.DefaultConcurrentTransfersCount;
+                    parallelTransferOptions.Value.MaximumThreadCount ?? Constants.Blob.Block.DefaultConcurrentTransfersCount;
                 var maximumBlockLength =
                     Math.Min(
                         Constants.Blob.Block.MaxStageBytes,
@@ -122,7 +123,7 @@ namespace Azure.Storage
                                 async,
                                 cancellationToken
                                 )
-                            .ConfigureAwait(false))
+                            )
                         {
                             // execute on background task
 
