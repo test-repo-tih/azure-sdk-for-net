ce09d86a2f255423c26e0720fc6dc748d2bd62d1
SDK Automation <sdkautomation@microsoft.com>
2019-10-09 03:10:34 +0000

    Generated from 18117ef45909ee7c4c1604c5a20cbd261eb5ea36
    example fixes

--- sdk/managednetwork/Microsoft.Azure.Management.ManagedNetwork/src/generated/SdkInfo_ManagedNetworkManagementClient.cs
+++ sdk/managednetwork/Microsoft.Azure.Management.ManagedNetwork/src/generated/SdkInfo_ManagedNetworkManagementClient.cs
@@ -27,16 +27,5 @@ namespace Microsoft.Azure.Management.ManagedNetwork
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/managednetwork/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\wd\\forkrepo\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "c98ed4c45dab31d5a949271c23b616c529d68b78";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
